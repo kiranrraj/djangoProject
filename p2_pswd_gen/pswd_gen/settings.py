a7bc19ef038a6a9d5dddf70f3885659a8702308c
Kiran Raj R <kiranrajvjd@gmail.com>
2020-10-04 02:02:55 +0530

    completed

--- p2_pswd_gen/pswd_gen/settings.py
+++ p2_pswd_gen/pswd_gen/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 import environ
 env = environ.Env()
 environ.Env.read_env()
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
