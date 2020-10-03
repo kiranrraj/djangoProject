005bd3c6fef573be6f5e87b857be63ada79ccc98
Kiran Raj R <kiranrajvjd@gmail.com>
2020-10-04 01:02:29 +0530

    logic completed

--- p2_pswd_gen/p_gen_app/views.py
+++ p2_pswd_gen/p_gen_app/views.py
@@ -2,28 +2,43 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import random
 
-def generate_pswd():
-    alpha = 'abcdefghijklmnopqrstuvwxyz'
-    number = '0123456789'
-    special = '!@#$%^&*()_?.'
-    numlist = list(number)
-    alphalist = list(alpha)
-    speciallist = list(special)
-    alphalistUpper = list(alpha.upper())
+# def generate_pswd():
+#     alpha = 'abcdefghijklmnopqrstuvwxyz'
+#     number = '0123456789'
+#     special = '!@#$%^&*()_?.'
+#     numlist = list(number)
+#     alphalist = list(alpha)
+#     speciallist = list(special)
+#     alphalistUpper = list(alpha.upper())
 
-def generateRand(request):
+def generatePswd(request):
     character = 'abcdefghijklmnopqrstuvwxyz'
+    uppercase = character.upper()
+    character = list('abcdefghijklmnopqrstuvwxyz')
+    number = '0123456789'
+    special = '!@#$%^&*()_'
     randpswd=""
+
     num = int(request.GET.get('length'))
+
+    if request.GET.get('upper'):
+        character.extend(uppercase)
+
+    if request.GET.get('number'):
+        character.extend(number)
+
+    if request.GET.get('special'):
+        character.extend(special)
+
     for i in range(int(num)):
         randpswd += random.choice(character)
     return randpswd
 
-def printReq(request):
-    upper = request.GET.get('upper')
-    number = request.GET.get('number')
-    special = request.GET.get('special')
-    return [upper, number, special]
+# def printReq(request):
+#     upper = request.GET.get('upper')
+#     number = request.GET.get('number')
+#     special = request.GET.get('special')
+#     return [upper, number, special]
 
 def home(request):
     return render(request, 'p_gen_app/home.html')
@@ -31,8 +46,8 @@ def home(request):
     print(request.GET())
 
 def result(request):
-    password = generateRand(request)
-    log = printReq(request)
-    return render(request, 'p_gen_app/result.html', {'password': password, 'log' : log})
+    password = generatePswd(request)
+    # log = printReq(request)
+    return render(request, 'p_gen_app/result.html', {'password':password})
     
 
